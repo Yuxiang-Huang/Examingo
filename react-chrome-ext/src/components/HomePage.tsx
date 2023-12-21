dc45b3c3d2e99578f85163146a7f72c74804e009
Yuxiang-Huang <yuxiangh142857@gmail.com>
2023-12-21 15:26:55 -0500

    Update navigations in home page

--- react-chrome-ext/src/components/HomePage.tsx
+++ react-chrome-ext/src/components/HomePage.tsx
@@ -1,25 +1,28 @@
 import { useNavigate } from "react-router-dom";
-import ExamingoLogo from './ExamingoLogo.png';
-import './HomePage.css';
+import ExamingoLogo from "./ExamingoLogo.png";
+import "./HomePage.css";
 
 const HomePage = () => {
   const navigate = useNavigate();
 
-  const goToPage1 = () => {
-    navigate("/Page1");
+  const gotToQuestionTypePage = () => {
+    navigate("/QuestionType");
   };
 
-  const goToPage2 = () => {
-    navigate("/Page2");
+  const goToResolveQuestionPage = () => {
+    navigate("/ResolveQuestion");
   };
 
   return (
     <div className="HomePage">
       <header className="Home-page-header">
-      <img src={ExamingoLogo} className="Logo" alt="logo" />
-      <div>HomePage</div>
-      <button onClick={goToPage1}>Page 1</button>
-      <button onClick={goToPage2}>Page 2</button>
+        <img src={ExamingoLogo} className="Logo" alt="logo" />
+        <div>HomePage</div>
+        <button onClick={gotToQuestionTypePage}>Test Yourself</button>
+        <button onClick={goToResolveQuestionPage}>Resolve Questions</button>
+        <button>
+          <a href="#">Personalize Learning</a>
+        </button>
       </header>
     </div>
   );
