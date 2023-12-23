0b6b5a082f2c25e6710a3b66a5d4beb8fcbb5bf9
Yuxiang-Huang <yuxiangh142857@gmail.com>
2023-12-22 21:01:29 -0500

    Add the custom colors

--- react-chrome-ext/tailwind.config.js
+++ react-chrome-ext/tailwind.config.js
@@ -2,6 +2,19 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    colors: {
+      "primary-red": "#FF001F",
+      "primary-purple": "#DB00FF",
+      "background-purple": "#290E2B",
+      "correct-green": "#42FF00",
+      "incorrect-red": "#FF001F",
+      "secondary-red": "#450000",
+      "secondary-purple": "#4C0059",
+      "primary-purple-50": "rgba(219,0,255,0.5)",
+      "primary-red-50": "rgba(255,0,31,0.5)",
+      "text-color": "white",
+    },
+
     extend: {},
   },
   plugins: [],
