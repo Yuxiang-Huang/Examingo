8d3fff618b83c521cb5244bb5e713e7e8f96f825
acee3 <alexjakhangcheung@gmail.com>
2023-12-23 15:52:42 -0500

    Made most of the progress on multiple choice button

--- react-chrome-ext/src/components/MultipleChoiceButton.tsx
+++ react-chrome-ext/src/components/MultipleChoiceButton.tsx
@@ -2,20 +2,45 @@ import React, { useState, useEffect } from 'react';
 
 interface MultipleChoiceButtonProps {
     optionText: string;
+    isCorrect: boolean;
+    isRevealed: boolean;
+    setIsRevealed: (newIsRevealed: boolean) => void;
 }
 
-const MultipleChoiceButton: React.FC<MultipleChoiceButtonProps> = ({  }) => {
-    const [selected, setSelected] = useState<boolean>();
-    const [incorrect, setIncorrect] = useState<boolean>();
-    const [correct, setCorrect] = useState<boolean>();
+const MultipleChoiceButton: React.FC<MultipleChoiceButtonProps> = ({ optionText, isCorrect, isRevealed, setIsRevealed }) => {
+    const [border, setBorder] = useState<string>("");
+    const [radioButtonStyling, setRadioButtonStyling] = useState<string>("");
+    
+    const revealChangeBorder = () => {
+        if (!isCorrect) {
+            setBorder("border-2 border-incorrect-red");
+        }
+        setIsRevealed(true);
+    }
+
+    const mouseOverHandler = () => {
+        if (isRevealed) return;
+        setBorder("border-2 border-primary-purple"); 
+        setRadioButtonStyling("bg-gradient-to-r from-primary-purple to-primary-red");
+    }
+
+    const mouseOutHandler = () => {
+        if (isRevealed) return;
+        setBorder(""); 
+        setRadioButtonStyling("");
+    }
+
     useEffect(() => {
-        // setEditable();
-    }, []);
+        if (isCorrect && isRevealed) {
+            setBorder("border-2 border-correct-green");
+        }
+    }, [isRevealed]);
 
     return (
-        <>
-            
-        </>
+        <div className={border + " " + "w-full h-12 relative flex flex-col justify-center rounded-xl my-2"} onClick={revealChangeBorder} onMouseOver={mouseOverHandler} onMouseOut={mouseOutHandler}>
+            <div className={radioButtonStyling + " " + "bg-black absolute  w-4 h-4 top-1/2 left-2 -translate-y-1/2 rounded-full"}></div>
+            <div className="ml-8">{optionText}</div>
+        </div>
     );
 };
 
--- react-chrome-ext/src/components/NavButton.tsx
+++ react-chrome-ext/src/components/NavButton.tsx
@@ -8,7 +8,7 @@ interface NavButtonProps {
 // Create a button with a custom svg shape with a gradient background
 const NavButton: React.FC<NavButtonProps> = ({ buttonText, clickFunction }) => {
     return (
-        <button onClick={clickFunction} className="w-9/12 relative m-2">
+        <button onClick={clickFunction} className="w-full relative my-2">
             <div className="absolute inset-0 bg-gradient-to-r from-primary-red to-primary-purple -skew-x-24 rounded-lg shadow-main"></div>
             <span className="relative">{buttonText}</span>
         </button>
