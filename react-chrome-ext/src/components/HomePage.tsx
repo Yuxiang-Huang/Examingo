227dec7f3ec70d41ab12fb5a64217a1e5da99f49
DiegoNieves1 <diegoxn19@gmail.com>
2023-12-23 16:36:29 -0500

    added personalize learning button

--- react-chrome-ext/src/components/HomePage.tsx
+++ react-chrome-ext/src/components/HomePage.tsx
@@ -44,7 +44,7 @@ const HomePage = () => {
   }
 
   return (
-    <div className="flex flex-col items-center justify-center">
+    <div className="flex flex-col items-center justify-center text-center">
       {/* <img src={ExamingoLogo} className="Logo" alt="logo" /> */}
       <LogoText />
       <NavButton
@@ -54,6 +54,11 @@ const HomePage = () => {
       <NavButton
         buttonText="Resolve Questions"
         clickFunction={goToResolveQuestionPage}
+      />
+
+      <NavButton
+        buttonText="Personalize Learning"
+        clickFunction={testReadibility}
       />
 
       <button className="underline m-4">
