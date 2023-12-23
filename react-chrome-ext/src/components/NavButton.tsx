ba2c81064cbc59a3b2acafee25c2d7a5fcd437ee
acee3 <alexjakhangcheung@gmail.com>
2023-12-22 23:06:59 -0500

    First update to NavButton.tsx

--- react-chrome-ext/src/components/NavButton.tsx
+++ react-chrome-ext/src/components/NavButton.tsx
@@ -1,16 +1,25 @@
 import React from "react";
 
 interface NavButtonProps {
-  buttonText: string;
-  clickFunction: () => void;
+    buttonText: string;
+    clickFunction: () => void;
 }
 
 const NavButton: React.FC<NavButtonProps> = ({ buttonText, clickFunction }) => {
-  return (
-    <button onClick={clickFunction} className=" border-2 p-2 m-4">
-      {buttonText}
-    </button>
-  );
+    return (
+        <button onClick={clickFunction} className=" w-full relative border-primary-red border-8">
+            <svg className="absolute" width='100%' height='100%' viewBox="0 0 100 100" preserveAspectRatio="none">
+                <defs>
+                    <linearGradient id="button-gradient">
+                        <stop offset="0%" stop-color="red" />
+                        <stop offset="100%" stop-color="purple" />
+                    </linearGradient>
+                </defs>
+                <polygon fill="url(#button-gradient)"points="0,0 0,100 100,100 100,0" />
+            </svg>
+            {buttonText}
+        </button>
+    );
 };
 
 export default NavButton;
