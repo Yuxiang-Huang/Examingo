b2fd2d262f74bac74f461ccaff2edadccec2c8cf
DiegoNieves1 <diegoxn19@gmail.com>
2023-12-22 22:49:04 -0500

    Just trying to pull right now

--- react-chrome-ext/src/components/LogoText.tsx
+++ react-chrome-ext/src/components/LogoText.tsx
@@ -1,11 +1,8 @@
 import React from 'react';
 
 const LogoText = () => {
-    return (
-        <>
-            
-        </>
-    );
+  return <div><span className="inline-block text-gradient-to-r from-primary-red to-primary-purple">Exam</span>
+  <span>ingo</span></div>;
 };
 
 export default LogoText;
