b0bd6f3fbfdc96d49aeec7fb3e5bcc43ffba394d
Yuxiang-Huang <yuxiangh142857@gmail.com>
2023-12-25 20:45:36 -0500

    Add functionality in the resolve question page

--- react-chrome-ext/src/components/pages/ResolveQuesion.tsx
+++ react-chrome-ext/src/components/pages/ResolveQuesion.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import CheckAnswerButton from "../CheckAnswerButton";
 import LogoText from "../LogoText";
 import Question from "../Question";
@@ -5,15 +6,26 @@ import SaveQuestionButton from "../SaveQuestionButton";
 import TextBox from "../TextBox";
 
 const ResolveQuesiton = () => {
+  const [display, setDisplay] = useState(false);
+
   return (
     <div>
       <LogoText />
       <Question questionText="Type Your Question" />
-      <TextBox initialText="" isReadOnly={false} textFunction={() => false} />
-      <CheckAnswerButton
-        buttonText="Answer"
-        checkAnswerFunction={() => false}
-      />
+      <TextBox initialText="" isReadOnly={display} textFunction={() => false} />
+      {display && (
+        <TextBox
+          initialText="I don't know."
+          isReadOnly={true}
+          textFunction={() => false}
+        />
+      )}
+      {!display && (
+        <CheckAnswerButton
+          buttonText="Answer"
+          checkAnswerFunction={() => setDisplay(true)}
+        />
+      )}
       <SaveQuestionButton saveFunction={() => false} />
     </div>
   );
