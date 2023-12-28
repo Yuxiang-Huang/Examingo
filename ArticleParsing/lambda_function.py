21b42cd8fcbad16d771ffdcdc44225c203ebf7e1
acee3 <alexjakhangcheung@gmail.com>
2023-12-27 22:08:07 -0500

    ArticleParsing thing

--- ArticleParsing/lambda_function.py
+++ ArticleParsing/lambda_function.py
@@ -1,8 +1,18 @@
-from newspaper import Article
+# from newspaper import Article
 from bs4 import BeautifulSoup
-# url = 'https://www.nytimes.com/2023/12/26/world/middleeast/israel-netanyahu-politics-mood.html'
-# url = 'https://www.sciencedirect.com/science/article/pii/S2352154620300097'
-# article = Article(url)
-# article.download()
-# article.parse()
-# print(article.text)
+# from transformers import pipeline
+from summarizer import Summarizer
+
+summarizer = Summarizer()
+# summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
+
+def lambda_handler(event, context):
+    html_doc = event['html_doc']
+    soup = BeautifulSoup(html_doc, "html.parser")
+    text = soup.get_text()
+    summarized_text = summarizer(text, num_sentences=3, max_length=150)
+    # summarized_text = summarizer(text, max_length=100, min_length=5, do_sample=False)
+    return {
+        "statusCode": 200, 
+        "body": summarized_text
+    }
