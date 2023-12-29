2bb46e10e17ccd6280dcb0d3b4fb7111d3521ee7
Yuxiang-Huang <yuxiangh142857@gmail.com>
2023-12-28 20:00:59 -0500

    Try to upload API to AWS...

---
+++ ModulesForAWS/index.mjs
@@ -0,0 +1,17 @@
+import express from "express";
+import { PrismaClient } from "@prisma/client";
+
+export const handler = async (event) => {
+  const prisma = new PrismaClient();
+  const app = express();
+
+  app.use(express.json());
+
+  const users = await prisma.user.findMany();
+
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify(users),
+  };
+  return response;
+};
