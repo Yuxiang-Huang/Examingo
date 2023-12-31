8dd1114ea3a0e2f55e76fb773776871332227a41
Yuxiang-Huang <yuxiangh142857@gmail.com>
2023-12-31 00:38:32 -0500

    Finally able to upload to AWS Lambda function

---
+++ PythonLambdaLayerTest/lambda_function.py
@@ -0,0 +1,13 @@
+from bs4 import BeautifulSoup
+
+from summa.summarizer import summarize
+
+
+def lambda_handler(event, context):
+    html_doc = event["html_doc"]
+    soup = BeautifulSoup(html_doc, "html.parser")
+    text = soup.get_text()
+    summarized_text = summarize(text, ratio=0.2)
+    # summarized_text = summarizer(text, num_sentences=3, max_length=150)
+    # summarized_text = summarizer(text, max_length=100, min_length=5, do_sample=False)
+    return {"statusCode": 200, "body": summarized_text}
